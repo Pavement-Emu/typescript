a9672d8685f2d0955a38b49a905e40816f211e2a
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-03-09 22:55:57 +0800

    complete exercise resistor-color-duo

--- exercises/practice/resistor-color-duo/resistor-color-duo.ts
+++ exercises/practice/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,30 @@
+const resistorMappings = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
+
 export class ResistorColor {
-  private colors: string[]
+  private colors: string[];
 
   constructor(colors: string[]) {
-    this.colors = colors
+    if (colors.length < 2) {
+      throw new Error("At least two colours need to be present");
+    }
+    this.colors = colors;
   }
-  value = (): number => 0
+  value = (): number => {
+    const resistorString = this.colors
+      .slice(0, 2)
+      .map((colour) => resistorMappings.indexOf(colour).toString())
+      .join("");
+    return Number.parseInt(resistorString, 10);
+  };
 }
