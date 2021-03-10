7df29c5d1673bf5caf514fa6319c3141565d0e40
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-03-10 21:51:44 +0800

    implement leap exercise

--- exercises/practice/leap/leap.ts
+++ exercises/practice/leap/leap.ts
@@ -1,5 +1,8 @@
-function isLeapYear(/* Parameters go here */) {
-  // Your code here
+function isLeapYear(year: number) {
+  const leapYear: boolean = year % 4 === 0;
+  const centenary: boolean = year % 100 === 0;
+  const fourthCentury: boolean = year % 400 === 0;
+  return (leapYear && !centenary) || fourthCentury;
 }
 
-export default isLeapYear
+export default isLeapYear;
