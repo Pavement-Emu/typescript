b0615cbf74d9ed04737f15aeb1d5ee99618db49b
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-03-10 22:56:15 +0800

    implement rna-transcription

--- exercises/practice/rna-transcription/rna-transcription.ts
+++ exercises/practice/rna-transcription/rna-transcription.ts
@@ -1,7 +1,21 @@
+function mapNucleotide(nucleotide: string) {
+  switch (nucleotide) {
+    case "C":
+      return "G";
+    case "G":
+      return "C";
+    case "A":
+      return "U";
+    case "T":
+      return "A";
+    default:
+      throw Error("Invalid input DNA.");
+  }
+}
 class Transcriptor {
-  toRna(/* Parameters go here */) {
-    // Your code here
+  toRna(dna: string) {
+    return dna.split("").map(mapNucleotide).join("");
   }
 }
 
-export default Transcriptor
+export default Transcriptor;
