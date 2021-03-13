51dcf9b33271748a27b71031a289417e39de34e0
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-03-13 13:53:50 +0800

    implement bob

--- exercises/practice/bob/bob.ts
+++ exercises/practice/bob/bob.ts
@@ -1,7 +1,25 @@
 class Bob {
-  hey(/* Parameters go here */) {
-    // Your code here
+  hey(message: string) {
+    message = message.trim();
+    const silence = message === "";
+    const containsCapitalLetters = message.toLowerCase() !== message;
+    const allUppercase = message.toUpperCase() === message;
+    const shouting = containsCapitalLetters && allUppercase;
+    const askingQuestion = message.endsWith("?");
+    if (shouting) {
+      if (askingQuestion) {
+        return "Calm down, I know what I'm doing!";
+      } else {
+        return "Whoa, chill out!";
+      }
+    } else if (askingQuestion) {
+      return "Sure.";
+    } else if (silence) {
+      return "Fine. Be that way!";
+    } else {
+      return "Whatever.";
+    }
   }
 }
 
-export default Bob
+export default Bob;
