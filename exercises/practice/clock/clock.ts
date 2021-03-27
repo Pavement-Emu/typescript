18771d4e31e2a218f776fa4a24c6ef2666cff133
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-03-27 14:26:43 +0800

    Clock - Refactor to store time in minutes only.

--- exercises/practice/clock/clock.ts
+++ exercises/practice/clock/clock.ts
@@ -1,74 +1,46 @@
+const DAY_IN_MINS: number = 60 * 24;
+
 export default class Clock {
-  minutes: number = 0;
-  hrs: number = 0;
+  timeInMinutes = 0;
   constructor(hrs: number, mins?: number) {
+    this.plus(hrs * 60);
     this.plus(mins || 0);
-    this.plusHrs(hrs);
-    // var minsNum = mins || 0;
-
-    // this.minutes = minsNum % 60;
-    // if (minsNum < 0) {
-    //   this.minutes += 60;
-    // }
-
-    // hrs += Math.floor(minsNum / 60);
-
-    // if (hrs < 0) {
-    //   hrs = 24 + (hrs % 24);
-    // }
-
-    // this.hrs = hrs % 24;
   }
 
   plus(mins: number): Clock {
-    if (mins < 0) {
-      return this.minus(Math.abs(mins));
-    }
-    this.minutes += mins;
-
-    if (this.minutes >= 60) {
-      this.plusHrs(Math.floor(this.minutes / 60));
-    }
-    this.minutes = this.minutes % 60;
-
+    this.timeInMinutes += mins;
+    this.normaliseTo24Hrs();
     return this;
+  }
+
+  private normaliseTo24Hrs(): number {
+    this.timeInMinutes %= DAY_IN_MINS;
+    if (this.timeInMinutes < 0) {
+      this.timeInMinutes += DAY_IN_MINS;
+    }
+    return this.timeInMinutes;
   }
 
   minus(mins: number): Clock {
-    this.minutes -= mins;
-    if (this.minutes < 0) {
-      this.minusHrs(Math.abs(Math.floor(this.minutes / 60)));
-      this.minutes %= 60;
-      this.minutes += 60;
-    }
-    return this;
-  }
-
-  private plusHrs(hrs: number) {
-    if (hrs < 0) {
-      return this.minusHrs(Math.abs(hrs));
-    }
-    this.hrs += hrs;
-    this.hrs %= 24;
-  }
-
-  private minusHrs(hrs: number) {
-    this.hrs -= hrs;
-    this.hrs %= 24;
-    if (this.hrs < 0) {
-      this.hrs += 24;
-    }
+    return this.plus(-mins);
   }
 
   equals(other: Clock): boolean {
-    const sameHr = this.hrs === other.hrs;
-    const sameMins = this.minutes === other.minutes;
-    return sameHr && sameMins;
+    return this.timeInMinutes === other.timeInMinutes;
   }
 
   toString(): string {
-    const hrs24 = this.hrs.toString().padStart(2, "0");
-    const min60 = this.minutes.toString().padStart(2, "0");
-    return `${hrs24}:${min60}`;
+    const pad = (num: number) => num.toString().padStart(2, "0");
+    const hrs24: string = pad(this.toHrs());
+    const min24: string = pad(this.toMins());
+    return `${hrs24}:${min24}`;
+  }
+
+  private toHrs(): number {
+    return Math.floor(this.timeInMinutes / 60);
+  }
+
+  private toMins(): number {
+    return this.timeInMinutes % 60;
   }
 }
