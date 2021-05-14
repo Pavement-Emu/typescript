f920485526003438d61b9c8eea297fd0e5eeb461
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-05-14 22:54:53 +0800

    word-search - partial implementation

--- exercises/practice/word-search/word-search.ts
+++ exercises/practice/word-search/word-search.ts
@@ -1,5 +1,16 @@
+type Found = {
+  [key: string]: undefined;
+};
+
+type Grid = string[];
+
 export default class WordSearch {
+  constructor(private grid: Grid) {}
   public find(words: string[]) {
-    throw new Error('Implement this function')
+    const found: Found = {};
+    for (const word in words) {
+      found[word] = undefined;
+    }
+    return found;
   }
 }
