d13e9f695a9fc893e3f890ef3fde38a48a4e36ca
Pavement-Emu <80335272+Pavement-Emu@users.noreply.github.com>
2021-05-25 23:24:22 +0800

    word-search: partial implementation - rows

--- exercises/practice/word-search/word-search.ts
+++ exercises/practice/word-search/word-search.ts
@@ -1,16 +1,55 @@
 type Found = {
-  [key: string]: undefined;
+  [key: string]: {} | undefined;
 };
 
 type Grid = string[];
 
 export default class WordSearch {
   constructor(private grid: Grid) {}
-  public find(words: string[]) {
-    const found: Found = {};
-    for (const word in words) {
-      found[word] = undefined;
+  public find(words: string[]): Found {
+    const join = (a: any, b: any) => {
+      return { ...a, ...b };
+    };
+    const matches = words.map(this.matches.bind(this)).reduce(join);
+    return matches;
+  }
+
+  private matches(word: string) {
+    return {
+      [word]: this.leftToRight(word) || this.rightToLeft(word)
+    };
+  }
+
+  private leftToRight(word: string) {
+    var lineNum = 0;
+    for (const line of this.grid) {
+      lineNum++;
+      for (var i = 0; i + word.length < line.length; i++) {
+        if (line.substr(i).startsWith(word)) {
+          return {
+            start: [lineNum, i + 1],
+            end: [lineNum, i + word.length]
+          };
+        }
+      }
     }
-    return found;
+  }
+
+  private rightToLeft(word: string) {
+    const reverse = (word: string) => word.split("").reverse().join("");
+    const reversedWord = reverse(word);
+    var lineNum = 0;
+    for (const line of this.grid) {
+      lineNum++;
+
+      for (var i = 0; i + reversedWord.length < line.length; i++) {
+        if (line.substr(i).startsWith(reversedWord)) {
+          return {
+            end: [lineNum, i + 1],
+            start: [lineNum, i + word.length]
+          };
+        }
+      }
+    }
   }
 }
